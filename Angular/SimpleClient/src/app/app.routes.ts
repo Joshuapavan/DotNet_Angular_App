f0021c601b2a5b3eb48b947772db185653abcae9
Joshua Pavan <joshuapavan35@gmail.com>
2025-06-20 11:09:47 +0530

    Adds auth gaurd

--- Angular/SimpleClient/src/app/app.routes.ts
+++ Angular/SimpleClient/src/app/app.routes.ts
@@ -5,13 +5,21 @@ import { MemberList } from './_components/members/member-list/member-list';
 import { MemberDetail } from './_components/members/member-detail/member-detail';
 import { Lists } from './_components/lists/lists';
 import { Messages } from './_components/messages/messages';
+import { authGuard } from './_gaurds/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'register', component: Register },
-  { path: 'members', component: MemberList },
-  { path: 'member/:username', component: MemberDetail },
-  { path: 'lists', component: Lists },
-  { path: 'messages', component: Messages },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'members', component: MemberList },
+      { path: 'member/:username', component: MemberDetail },
+      { path: 'lists', component: Lists },
+      { path: 'messages', component: Messages },
+    ],
+  },
   { path: '**', component: Home, pathMatch: 'full' },
 ];
