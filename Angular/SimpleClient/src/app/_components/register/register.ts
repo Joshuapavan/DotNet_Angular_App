64613e46476150de1c2727607390c35207c68caa
G Pavan Kumar <pavan.kumar@magicsw.com>
2025-06-30 15:33:10 +0530

    Adds register functionality using reactive forms

--- Angular/SimpleClient/src/app/_components/register/register.ts
+++ Angular/SimpleClient/src/app/_components/register/register.ts
@@ -1,13 +1,14 @@
 import { Component, inject, input, OnInit, output, signal } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { Account } from '../../_services/account';
 import { ToastrService } from 'ngx-toastr';
 import { Router, RouterLink } from '@angular/router';
-import { JsonPipe, NgIf } from '@angular/common';
+import { TextInput } from "../_forms/text-input/text-input";
+import { DatePicker } from "../_forms/date-picker/date-picker";
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule, RouterLink, JsonPipe, NgIf],
+  imports: [ReactiveFormsModule, RouterLink, TextInput, DatePicker],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
@@ -15,28 +16,37 @@ export class Register implements OnInit {
   accountService = inject(Account);
   private toastr = inject(ToastrService);
   private router = inject(Router);
+  // Forms builder
+  private fb = inject(FormBuilder);
+
+  // Date
+  maxDate = new Date();
 
   cancelRegister = output<boolean>();
-  validationErrors: Array<String> = [];
+  validationErrors: Array<String | undefined> = [];
 
   showPassword = signal<boolean>(false);
   showConfirmPassword = signal<boolean>(false);
 
   registerForm: FormGroup = new FormGroup({})
 
-  model: any = {};
-
-
   ngOnInit(): void {
     this.initilaizeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18)
   }
 
   initilaizeForm(){
-    this.registerForm = new FormGroup(
+    this.registerForm = this.fb.group(
       {
-        username: new FormControl('', Validators.required),
-        password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-        confirmPassword: new FormControl('', [Validators.required, this.isMatching('password')]),
+        gender: ['male', Validators.required],
+        knownAs: ['', Validators.required],
+        city: ['', Validators.required],
+        country: ['', Validators.required],
+        dateOfBirth: ['', Validators.required],
+
+        username: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+        confirmPassword: ['', [Validators.required, this.isMatching('password')]],
       }
     );
 
@@ -52,16 +62,16 @@ export class Register implements OnInit {
   }
 
   register() {
-    this.accountService.register(this.model).subscribe({
+    const dob = this.getDateOnly(this.registerForm.get('dateOfBirth')?.value);
+    this.registerForm.patchValue({dateOfBirth: dob});
+    this.accountService.register(this.registerForm.value).subscribe({
       next: (response) => {
+        this.router.navigateByUrl('/home')
         this.toastr.success('Welcome ,' + response.username);
         this.cancel();
       },
       error: (error) => {
-        if (Array.isArray(error)) {
-          this.toastr.error('Error while Signing up.');
-          this.validationErrors = error;
-        }
+       this.validationErrors = error
       },
     });
   }
@@ -77,5 +87,10 @@ export class Register implements OnInit {
   cancel() {
     this.cancelRegister.emit(false);
     this.router.navigateByUrl('/login');
+  }
+
+  private getDateOnly(dob: string | undefined){
+    if(!dob) return;
+    return new Date(dob).toISOString().slice(0,10);
   }
 }
